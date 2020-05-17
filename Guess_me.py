f883176debb1a46496d1f7a187784504dfb75a59
Mahmoud salah Mohamed <44245137+mahmoud24598salah@users.noreply.github.com>
2020-05-17 09:23:10 +0200

    guessing game in python by while loop

---
+++ Guess_me.py
@@ -0,0 +1,14 @@
+secret_number=9
+guess_count=0
+guess_limit=3
+while guess_count < guess_limit:
+    guess=int(input('guess :'))
+    guess_count=guess_count+1
+    if guess==secret_number:
+        print('you win')
+        break
+    else:
+        if guess_limit-guess_count==0:
+            print('you faild')
+        else:
+            print('enter anohter number you has '+str(guess_limit-guess_count)+' chance')
